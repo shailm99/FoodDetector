f756dc3f7f6b3d93cd116bb4e964a0a7eebfd436
Shail Mirpuri <mirpurishail@gmail.com>
2020-12-29 10:34:10 +0800

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [general]\n\
-email = \"mirpurishail@gmail.com\"\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
-
