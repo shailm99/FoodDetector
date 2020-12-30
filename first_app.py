e721f1e7a33c0f94147512c92f9c4722376ddb17
Shail Mirpuri <68639536+shailm99@users.noreply.github.com>
2020-12-30 11:56:11 +0800

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -17,15 +17,16 @@ def main():
     
     st.write("#### Add a picture of what you're eating at the moment, and we will tell you what it is")
     
-    image = None
+    image = None # intialize a variable to store the image
     
+    # Ask the user for their information so we can generate their calories required
     gender = st.selectbox('Your Gender/Sex', ['Male', 'Female', 'Gender Diverse'])
     age = st.slider('Age', 0, 110, 20)
     weight = st.slider("Current Weight In Pounds", 0, 300, 150)
     height = st.slider("Current Height in Cm", 0, 250, 140)
     activity = st.selectbox("Your Activity Level", ["High", "Medium", "Low"])
     
-    multipler = {"High" : 2.25, "Medium" : 1.76, "Low" : 1.53}
+    multipler = {"High" : 2.25, "Medium" : 1.76, "Low" : 1.53} # The Basal multipler based upon the activity level(Harris–Benedict BMR)
     
     act_multipler = multipler[activity]
     
@@ -39,24 +40,31 @@ def main():
         calories_needed = act_multipler * (-75  + (4.5 * weight) + (6.25 * height) - (5 * age))
     
     
+    # Allow the user to upload a image of their meal
     image = st.file_uploader("The image of your meal!", ["png", "jpg", "jpeg", 'HEIC'], key = 'file')
     
-    model = load_model('vgg19.h', compile = False)
     
+    # Load and Compile the Model
+    model = load_model('vgg19.h', compile = False)
     loss = 'sparse_categorical_crossentropy'
     optimizer = 'adam'
     model.compile(loss = loss, optimizer = optimizer)
     
+    # Open the labels.text, which stores the class names 
     labels = open('labels.txt')
     classes = labels.read()
     classes = classes.split(sep = '\n')
     classes = classes[:-1]
     labels.close()
     
+    # read the nutritional facts dataframe
     nutri_df = pd.read_csv('label_nutrients.csv', index_col = 0)
     
     
     def make_prediction(img, model = model):
+       '''
+        This function takes in a image and model, and uses the model to predict the class of the image 
+       '''
        img = img.convert('RGB')
        img = img.resize((224,224), Image.NEAREST)
        img = tf.keras.preprocessing.image.img_to_array(img)
@@ -64,14 +72,15 @@ def main():
        prediction = model.predict(img_array) 
        pred_class = np.argmax(prediction) 
        return classes[pred_class]
-    
-    if image is not None:
-        col1, col2 = st.beta_columns(2)
-        st.balloons()
-        with col1:
+   
+
+    if image is not None: # if the image is an actual file then
+        col1, col2 = st.beta_columns(2) # split our layout into two columns
+        st.balloons() # display the ballons
+        with col1: # the first column in our layout will display the image
             image_to_share = Image.open(image)
             st.image(image_to_share, width=265)
-        with col2:
+        with col2: # the second column will display the predicted class, nutritional facts and reccomended calories remaining
             st.write("## The Predicted Class Is:")
             predicted_class = make_prediction(image_to_share)
             st.write('# {}'.format(predicted_class))
